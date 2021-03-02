3761ec4549e3a9958a3b73f56b02faaf3336e80d
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-03 03:03:17 +0530

    use custom layout for MaterialEditText, use it in sample app

--- MaterialEditText/src/main/java/com/isolpro/library/materialedittext/MaterialEditText.java
+++ MaterialEditText/src/main/java/com/isolpro/library/materialedittext/MaterialEditText.java
@@ -2,6 +2,9 @@ package com.isolpro.library.materialedittext;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,9 +12,12 @@ import androidx.annotation.Nullable;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MaterialEditText extends TextInputLayout {
+public class MaterialEditText extends RelativeLayout {
 
   private final Context context;
+
+  private View mv;
+  private TextInputLayout textInputLayout;
   private TextInputEditText textInputEditText;
 
   public MaterialEditText(@NonNull Context context) {
@@ -32,12 +38,14 @@ public class MaterialEditText extends TextInputLayout {
   }
 
   private void instantiate() {
-    textInputEditText = new TextInputEditText(context);
+    mv = LayoutInflater.from(context).inflate(R.layout.material_edit_text, null);
+
+    textInputLayout = mv.findViewById(R.id.til);
+    textInputEditText = mv.findViewById(R.id.tiet);
   }
 
   private void initialize() {
-    addView(textInputEditText);
-
-//    setBoxBackgroundMode(BOX_BACKGROUND_OUTLINE);
+    addView(mv);
   }
+
 }
